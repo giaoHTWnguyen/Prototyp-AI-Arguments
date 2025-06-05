7dc65fbe50b3b04af15ce477c66b92c175275749
Giao Nguyen <s0573654@htw-berlin.de>
2025-06-05 17:41:20 +0200

    Überarbeitung Messages

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
 from openai import OpenAI
+from openai import APIConnectionError, AuthenticationError, InternalServerError
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = OpenAI(api_key=os.getenv("OPEN_API_KEY"))
+try:
 
-response = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", 
-         "content": """Du bist ein KI-Tutor, der formatives Feedback zu argumentativem Schreiben geben soll. 
-         Das Ziel ist es, dem Lernenden zu helfen, ihre Prägnanz, Logik und Überzeugungskraft zu verbessern. Gebe stets 
-         umsetzbare, klare und ermutigende Ratschläge
-         """}
-    ],
-    response_format={"type": "json_object"}
-)
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-print(response.choices[0].message.content)
+    response = client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", 
+            "content": """Du bist ein KI-Tutor, der formatives Feedback zu argumentativem Schreiben geben soll. 
+            Das Ziel ist es, dem Lernenden zu helfen, ihre Prägnanz, Logik und Überzeugungskraft zu verbessern. Gebe stets 
+            umsetzbare, klare und kritische Ratschläge
+            """},
+            {"role": "user", "content": """Ich denke die Regierung sollte den Einsatz von Künstlicher Intelligenz regulieren, weil ich Angst habe, dass 
+             die Menschheit auslöscht durch Skynet wie in Terminator.
+             """}
+        ],
+    )
+
+    print(response.choices[0].message.content)
+
+except AuthenticationError as e:
+    print(f"\nAuthentifizierungsfehler: Dein API-Schlüssel ist wahrscheinlich ungültig oder widerrufen.")
+    print(f"Details: {e}")
+except APIConnectionError as e:
+    print(f"\nVerbindungsfehler: Konnte keine Verbindung zur OpenAI API herstellen.")
+    print(f"Details: {e}")
+except InternalServerError as e:
+    print(f"\nInternalServerError")
+    print(f"Details: {e}")
+except Exception as e:
+    print(f"\nEin unerwarteter Fehler ist aufgetreten:")
+    print(f"Details: {e}")
+
