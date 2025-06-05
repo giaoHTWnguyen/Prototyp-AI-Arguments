fe6deb56c152fe0e2d7cbbe3ca9b9dc941be23a8
Giao Nguyen <s0573654@htw-berlin.de>
2025-06-05 19:37:15 +0200

    Adde messageSystem und messageUser

--- main.py
+++ main.py
@@ -5,24 +5,25 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+messageSystem = {"role": "system", 
+            "content": """Du bist ein KI-Tutor, der Formatives Feedback zu argumentativem Schreiben geben soll. 
+            Das Ziel ist es, dem Lernenden zu helfen, ihre Prägnanz, Logik und Überzeugungskraft zu verbessern. Gebe stets 
+            umsetzbare, klare und kritische Ratschläge
+            """}
+
+messageUser = {"role": "user", "content": """Ich denke die Regierung sollte den Einsatz von Künstlicher Intelligenz regulieren, weil ich Angst habe, dass 
+             die Menschheit auslöscht durch Skynet wie in Terminator.
+             """}
+
 try:
-
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", 
-            "content": """Du bist ein KI-Tutor, der formatives Feedback zu argumentativem Schreiben geben soll. 
-            Das Ziel ist es, dem Lernenden zu helfen, ihre Prägnanz, Logik und Überzeugungskraft zu verbessern. Gebe stets 
-            umsetzbare, klare und kritische Ratschläge
-            """},
-            {"role": "user", "content": """Ich denke die Regierung sollte den Einsatz von Künstlicher Intelligenz regulieren, weil ich Angst habe, dass 
-             die Menschheit auslöscht durch Skynet wie in Terminator.
-             """}
+            messageSystem,
+            messageUser
         ],
     )
-
     print(response.choices[0].message.content)
 
 except AuthenticationError as e:
