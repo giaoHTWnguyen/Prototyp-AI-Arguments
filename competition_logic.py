f7540a3df077093a52a4dc03d009fe2597f85f49
Giao Nguyen <s0573654@htw-berlin.de>
2025-06-20 00:44:15 +0200

    Auskommentieren Choose Question

--- competition_logic.py
+++ competition_logic.py
@@ -26,24 +26,24 @@ discussion_questions_categorized = {
     ]
 }
 
-#Choose_question: Funktion wird nicht mehr genutzt --> iteration aller Frage im Terminal
-def choose_question() -> str: #Algorithmus zur Auswahl einer Frage aus dem Diskussionskatalog
-    print("\n--- Verfügbare Diskussionsfragen ---")
-    for i, question in enumerate(discussion_questions): #Iteration durch den Katalog, es wird eine ID zu jeder Frage gemappt für schnelleren Zugriff
-        print(f"{i+1}. {question}")
-    print("------------------------------")
+#Choose_question: Funktion wird nicht mehr genutzt!!! --> iteration aller Frage im Terminal
+# def choose_question() -> str: #Algorithmus zur Auswahl einer Frage aus dem Diskussionskatalog
+#     print("\n--- Verfügbare Diskussionsfragen ---")
+#     for i, question in enumerate(discussion_questions): #Iteration durch den Katalog, es wird eine ID zu jeder Frage gemappt für schnelleren Zugriff
+#         print(f"{i+1}. {question}")
+#     print("------------------------------")
 
-    while True:
-        try:
-            chosen_id = int(input("\nBitte wähle die Nummer der Diskussionsfrage aus: "))
-            if 1 <= chosen_id <= len(discussion_questions):
-                choose_question = discussion_questions[chosen_id-1]
-                print(f"\nDu hast gewählt: '{choose_question}'")
-                return choose_question
-            else:
-                print("Ungültige Wahl. Bitte eine Nummer aus der Liste eingeben.")
-        except ValueError:
-            print("Ungültige Eingabe. Bitte gib eine Zahl ein")
+#     while True:
+#         try:
+#             chosen_id = int(input("\nBitte wähle die Nummer der Diskussionsfrage aus: "))
+#             if 1 <= chosen_id <= len(discussion_questions):
+#                 choose_question = discussion_questions[chosen_id-1]
+#                 print(f"\nDu hast gewählt: '{choose_question}'")
+#                 return choose_question
+#             else:
+#                 print("Ungültige Wahl. Bitte eine Nummer aus der Liste eingeben.")
+#         except ValueError:
+#             print("Ungültige Eingabe. Bitte gib eine Zahl ein")
 
 def get_user_argumentation() -> str:
     print("\nBitte schreibe deine Argumentation:")
